cd08a05b2e133d6210a98c893804d389513a637d
Akoruu.Dev <akoruu.dev@gmail.com>
2022-12-20 18:34:53 -0300

    FEAT [Urls]: Create url shorten controller from API

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -1,23 +1,45 @@
+import { nanoid } from "nanoid";
+import { db } from "../database/database.js";
+
 export async function shorten( req, res ) {
     const { url, userId } = res.locals.body;
+    const short = nanoid(10);
+    
+    try {
+        await db.query(`INSERT INTO urls ("userId", url, cut) VALUES ($1, $2, $3);`, [userId, url, short]);
 
-    return res.status(201).send({ userId, url });
+        return res.status(201).send({ shortUrl: short });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 };
 
 export async function getId( req, res ) {
     const {} = req.body;
 
-    return res.status(201).send();
+    try {
+        return res.status(201).send({ shortUrl: short });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 };
 
 export async function shortUrl( req, res ) {
     const {} = req.body;
 
-    return res.status(201).send();
+    try {
+        return res.status(201).send({ shortUrl: short });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 };
 
 export async function deleteId( req, res ) {
     const {} = req.body;
 
-    return res.status(201).send();
+    try {
+        return res.status(201).send({ shortUrl: short });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 };
